da5d8f70142ef736aed33cbf0dcb46345dd17594
Allan Ecker <maskedretriever@gmail.com>
2017-01-21 17:14:48 -0800

    gpio in C from scratch is now working

--- C/gpio/gpio_blink.c
+++ C/gpio/gpio_blink.c
@@ -5,12 +5,14 @@ main()
 {
     char * p_PortB = (char *)P_PORTB;
     char * p_DirB = (char *)P_DDRB;
+    volatile int i=0;
 
     *p_DirB = 0x01;
 
     while(1)
     {
-        *p_PortB=0x0;
+	for(i=0;i<20000;i++);
+        *p_PortB^=0x1;
     }
 
 }
